a989ed30b911eabf3221891f3835253a509029d3
Lee-Han-Sol <107240538+Lee-Han-Sol@users.noreply.github.com>
2022-11-11 19:53:19 +0900

    완주하지 못한 선수

---
+++ hash/1.java
@@ -0,0 +1,25 @@
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+String answer = "";
+        HashMap<String, Integer> map = new HashMap<>();
+        for (String player : participant) 
+            map.put(player, map.getOrDefault(player, 0) + 1);
+        for (String player : completion) 
+            map.put(player, map.get(player) - 1);
+
+            Iterator<Map.Entry<String, Integer>> iter = map.entrySet().iterator();
+
+        while(iter.hasNext()){
+            Map.Entry<String, Integer> entry = iter.next();
+            if (entry.getValue() != 0){
+                answer = entry.getKey();
+                break;
+            }
+        }
+        return answer;
+    }
+}
