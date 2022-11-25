1b94f6d1ddfd42e9c5017552cdf78b7518de8cab
Lee-Han-Sol <107240538+Lee-Han-Sol@users.noreply.github.com>
2022-11-25 18:46:23 +0900

    다음 큰 숫자

---
+++ practice/7-er.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int solution(int n) {
+        int answer = n+1;
+        //An < An+1
+        //An 과 An+1 의 2진 1의 갯수는 같다.
+        
+        String binaryStringN = Integer.toBinaryString(n);
+        binaryStringN = binaryStringN.replaceAll("0","");
+        String binaryStringA = Integer.toBinaryString(answer);
+        
+        
+        while(binaryStringN.length()!=binaryStringA.length()){
+            
+            binaryStringA = Integer.toBinaryString(answer);
+            binaryStringA = binaryStringA.replaceAll("0","");
+            
+            if(binaryStringN.length()==binaryStringA.length()){
+                break;
+            }
+            
+            answer++;
+            
+        }
+        
+        return answer;
+        
+    }
+    
+}
+
+//효율성에서 문제가 되고 있는중
