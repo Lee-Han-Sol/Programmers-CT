4b954deff37b92bf40b2c536ad8bddb005e1dd6a
Lee-Han-Sol <107240538+Lee-Han-Sol@users.noreply.github.com>
2023-01-01 22:09:30 +0900

    도둑질

---
+++ Dynamic Programing(점화식)/5.java
@@ -0,0 +1,14 @@
+    public int solution(int[] money) {
+        int len = money.length;
+        int[] dpInclude0 = new int[len];
+        int[] dpExclude0 = new int[len];
+        dpInclude0[0] = dpInclude0[1] = money[0];
+        dpExclude0[1] = money[1];
+
+        for (int i = 2; i < len; i++) {
+            dpInclude0[i] = Math.max(dpInclude0[i-1], money[i] + dpInclude0[i-2]);
+            dpExclude0[i] = Math.max(dpExclude0[i-1], money[i] + dpExclude0[i-2]);
+        }
+
+        return Math.max(dpInclude0[len-2], dpExclude0[len-1]);
+    }
