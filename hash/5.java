b7e9d67dfec4bad7d071dc02aac6b45483746a66
Lee-Han-Sol <107240538+Lee-Han-Sol@users.noreply.github.com>
2023-01-27 20:52:29 +0900

    베스트 앨범

--- hash/5.java
+++ hash/5.java
@@ -1,27 +1,55 @@
 import java.util.*;
 
 //내 코드
+import java.util.*;
+ 
 class Solution {
     public int[] solution(String[] genres, int[] plays) {
-        int[] answer = {};
-        HashMap<String,Integer> total = new HashMap<String,Integer>();
-        for(int i = 0 ; i< genres.length; i++){
-            if(total.get(genres[i])!=null){
-                total.put(genres[i],total.get(genres[i])+plays[i]);
-            }else{
-                total.put(genres[i],plays[i]);
-            }
-	/* 위 코드는 아래처럼 가능
-	for(int i=0; i<genres.length; i++){
-            total.put(genres[i], total.getOrDefault(genres[i], 0)+plays[i]);
-        }
-	*/
-	List<String> sort = new ArrayList<>(total.keySet()); // 판매량 순 
-        System.out.println(sort);
-		
+        Map<String, Integer> map = new HashMap<>();
+        for(int i = 0; i < genres.length; i++) {
+            map.put(genres[i], map.getOrDefault(genres[i], 0) + plays[i]);
         }
         
-        return answer;
+        //key값만 가져와서 genre에 넣어준다
+        ArrayList<String> genre = new ArrayList<>();
+        for(String key : map.keySet()) {
+            genre.add(key);
+        }
+        Collections.sort(genre, (o1, o2) -> map.get(o2) - map.get(o1)); //key값에 해당하는 value를 내림차순으로 정렬한다.
+        
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i = 0; i < genre.size(); i++) {
+            String g = genre.get(i);
+            
+            //해당 장르의 음악 중에서 play횟수가 가장 큰 인덱스를 찾는다
+            int max = 0;
+            int firstIdx = -1;
+            for(int j = 0; j < genres.length; j++) {
+                if(g.equals(genres[j]) && max < plays[j]) {
+                    max = plays[j];
+                    firstIdx = j;
+                }
+            }
+            
+            //해당 장르의 음악 중에서 play횟수가 두번째로 큰 인덱스를 찾는다.
+            max = 0;
+            int secondIdx = -1;
+            for(int j = 0; j < genres.length; j++) {
+                if(g.equals(genres[j]) && max < plays[j] && j != firstIdx) { 
+                    max = plays[j];
+                    secondIdx = j;
+                }
+            }
+            
+            list.add(firstIdx);
+            if(secondIdx >= 0) list.add(secondIdx); //secondIdx는 존재 할수도, 안할수도 있다.
+        }
+        
+        int[] result = new int[list.size()];
+        for(int i = 0; i < list.size(); i++) {
+            result[i] = list.get(i);
+        }
+        return result;
     }
 }
 
