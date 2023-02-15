83b55e141d81f98844108763b6833dad87d45bb3
Lee-Han-Sol <107240538+Lee-Han-Sol@users.noreply.github.com>
2023-02-15 20:21:50 +0900

    호텔 대실

--- practice/19.java
+++ practice/19.java
@@ -1,1 +1,27 @@
+import java.util.*;
+class Solution {
+    public int solution(String[][] book_time) {
+        PriorityQueue<Integer> in = new PriorityQueue<>();
+        PriorityQueue<Integer> out = new PriorityQueue<>();
+        int max=0, cnt=1;
+        for(String[] time : book_time) {
+            in.offer(helper(time[0]));
+            out.offer(helper(time[1])+10);
+        }
+        while(!out.isEmpty() && !in.isEmpty()) {
+            int o=out.poll();
+            cnt--;
+            while(!in.isEmpty() && in.peek()<o) {
+                in.poll();
+                cnt++;
+            }
+            max = Math.max(max, cnt);
+        }
+        return max;
+    }
 
+    public int helper(String str) {
+        String[] s = str.split(":");
+        return Integer.valueOf(s[1])+Integer.valueOf(s[0])*60;
+    }
+}
